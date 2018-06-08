6aa5251035992059c2078c662904ba98777d4d22
oprisan <aoprisan@adobe.com>
2018-06-08 16:49:16 +0300

    Extracted some variables

--- Tests/run_multiply_tests.sh
+++ Tests/run_multiply_tests.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 MPI_PROCESSES=7
+MATRIX_DIMENSION_START=10
+MAX_MATRIX_DIMENSION=100
+MATRIX_DIMENSION_STEP=10
 
-for (( MATRIX_DIMENSION=10; MATRIX_DIMENSION<=100; MATRIX_DIMENSION+=10 )); do
+for (( MATRIX_DIMENSION=${MATRIX_DIMENSION_START}; MATRIX_DIMENSION<=${MAX_MATRIX_DIMENSION}; MATRIX_DIMENSION+=${MATRIX_DIMENSION_STEP} )); do
     echo "===================================================================================================================================================="
     echo "Testing with square matrix of dimension $MATRIX_DIMENSION and $MPI_PROCESSES number of MPI processes"
     echo
