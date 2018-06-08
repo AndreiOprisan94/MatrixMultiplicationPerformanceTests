c24e28de101244b083a8efe1aec2bc9b4e196a52
oprisan <aoprisan@adobe.com>
2018-06-08 16:23:19 +0300

    Launched the programs

--- Tests/run_multiply_tests.sh
+++ Tests/run_multiply_tests.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "Hello There!"
+MPI_PROCESSES=7
+MATRIX_DIMENSION=10
+
+echo "Testing with square matrix of dimension $MATRIX_DIMENSION and $MPI_PROCESSES number of MPI processes"
+echo
+
+time $SERIAL_MULTIPLYER ${MATRIX_DIMENSION}
+echo
+echo
+time mpirun --oversubscribe -np ${MPI_PROCESSES} $PARALLEL_MULTIPLYER ${MATRIX_DIMENSION}
+
+echo "Test finished!"
