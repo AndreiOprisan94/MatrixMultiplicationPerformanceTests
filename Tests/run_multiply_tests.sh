d5453181a12cd5cb5507d772954aa9ae977582f4
oprisan <aoprisan@adobe.com>
2018-06-08 16:43:50 +0300

    Added loop for testing

--- Tests/run_multiply_tests.sh
+++ Tests/run_multiply_tests.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 MPI_PROCESSES=7
-MATRIX_DIMENSION=10
 
-echo "Testing with square matrix of dimension $MATRIX_DIMENSION and $MPI_PROCESSES number of MPI processes"
-echo
+for (( MATRIX_DIMENSION=10; MATRIX_DIMENSION<=100; MATRIX_DIMENSION+=10 )); do
+    echo "===================================================================================================================================================="
+    echo "Testing with square matrix of dimension $MATRIX_DIMENSION and $MPI_PROCESSES number of MPI processes"
+    echo
 
-time $SERIAL_MULTIPLYER ${MATRIX_DIMENSION}
-echo
-echo
-time mpirun --oversubscribe -np ${MPI_PROCESSES} $PARALLEL_MULTIPLYER ${MATRIX_DIMENSION}
+    time $SERIAL_MULTIPLYER ${MATRIX_DIMENSION}
+    echo
+    echo
+    time mpirun --oversubscribe -np ${MPI_PROCESSES} $PARALLEL_MULTIPLYER ${MATRIX_DIMENSION}
+
+    echo
+    echo
+done
 
 echo "Test finished!"
